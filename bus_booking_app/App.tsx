0a2bfd9389fc1000c3ec6def0c818d3bbf415588
Chirag Bellani <chiragbellani@Chirags-MacBook-Air.local>
2025-10-14 00:01:48 +0530

    Initial commit with backend and frontend setup

---
+++ bus_booking_app/App.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Navigation from './src/navigation/Navigation'
+import './global.css'
+const App = () => {
+  return (
+    <Navigation/>
+  )
+}
+
+export default App
